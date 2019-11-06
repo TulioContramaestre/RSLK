0304c4099c2aff21f0c03970e9a844db480564d6
tu671970 <56061986+tu671970@users.noreply.github.com>
2019-11-05 19:53:24 -0500

    Add files via upload

---
+++ Lab8/Debug/Library/Reflectance.d
@@ -0,0 +1,41 @@
+# FIXED
+
+Library/Reflectance.obj: ../Library/Reflectance.c
+Library/Reflectance.obj: D:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/stdint.h
+Library/Reflectance.obj: D:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/_stdint40.h
+Library/Reflectance.obj: D:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/stdint.h
+Library/Reflectance.obj: D:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/cdefs.h
+Library/Reflectance.obj: D:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/_types.h
+Library/Reflectance.obj: D:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/machine/_types.h
+Library/Reflectance.obj: D:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/machine/_stdint.h
+Library/Reflectance.obj: D:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/_stdint.h
+Library/Reflectance.obj: D:/ti/ccs910/ccs/ccs_base/arm/include/msp432.h
+Library/Reflectance.obj: D:/ti/ccs910/ccs/ccs_base/arm/include/msp432p401r.h
+Library/Reflectance.obj: D:/ti/ccs910/ccs/ccs_base/arm/include/msp_compatibility.h
+Library/Reflectance.obj: D:/ti/ccs910/ccs/ccs_base/arm/include/msp432p401r_classic.h
+Library/Reflectance.obj: C:/ti/simplelink_msp432p4_sdk_3_20_00_06/source/third_party/CMSIS/Include/core_cm4.h
+Library/Reflectance.obj: C:/ti/simplelink_msp432p4_sdk_3_20_00_06/source/third_party/CMSIS/Include/cmsis_compiler.h
+Library/Reflectance.obj: C:/ti/simplelink_msp432p4_sdk_3_20_00_06/source/third_party/CMSIS/Include/cmsis_ccs.h
+Library/Reflectance.obj: D:/ti/ccs910/ccs/ccs_base/arm/include/system_msp432p401r.h
+Library/Reflectance.obj: ../Library/Clock.h
+Library/Reflectance.obj: ../Library/Reflectance.h
+
+../Library/Reflectance.c: 
+D:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/stdint.h: 
+D:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/_stdint40.h: 
+D:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/stdint.h: 
+D:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/cdefs.h: 
+D:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/_types.h: 
+D:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/machine/_types.h: 
+D:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/machine/_stdint.h: 
+D:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/_stdint.h: 
+D:/ti/ccs910/ccs/ccs_base/arm/include/msp432.h: 
+D:/ti/ccs910/ccs/ccs_base/arm/include/msp432p401r.h: 
+D:/ti/ccs910/ccs/ccs_base/arm/include/msp_compatibility.h: 
+D:/ti/ccs910/ccs/ccs_base/arm/include/msp432p401r_classic.h: 
+C:/ti/simplelink_msp432p4_sdk_3_20_00_06/source/third_party/CMSIS/Include/core_cm4.h: 
+C:/ti/simplelink_msp432p4_sdk_3_20_00_06/source/third_party/CMSIS/Include/cmsis_compiler.h: 
+C:/ti/simplelink_msp432p4_sdk_3_20_00_06/source/third_party/CMSIS/Include/cmsis_ccs.h: 
+D:/ti/ccs910/ccs/ccs_base/arm/include/system_msp432p401r.h: 
+../Library/Clock.h: 
+../Library/Reflectance.h: 
---
+++ Lab8/Debug/ccs/startup_msp432p401r_ccs.d
@@ -0,0 +1,21 @@
+# FIXED
+
+ccs/startup_msp432p401r_ccs.obj: ../ccs/startup_msp432p401r_ccs.c
+ccs/startup_msp432p401r_ccs.obj: D:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/stdint.h
+ccs/startup_msp432p401r_ccs.obj: D:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/_stdint40.h
+ccs/startup_msp432p401r_ccs.obj: D:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/stdint.h
+ccs/startup_msp432p401r_ccs.obj: D:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/cdefs.h
+ccs/startup_msp432p401r_ccs.obj: D:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/_types.h
+ccs/startup_msp432p401r_ccs.obj: D:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/machine/_types.h
+ccs/startup_msp432p401r_ccs.obj: D:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/machine/_stdint.h
+ccs/startup_msp432p401r_ccs.obj: D:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/_stdint.h
+
+../ccs/startup_msp432p401r_ccs.c: 
+D:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/stdint.h: 
+D:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/_stdint40.h: 
+D:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/stdint.h: 
+D:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/cdefs.h: 
+D:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/_types.h: 
+D:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/machine/_types.h: 
+D:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/machine/_stdint.h: 
+D:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/_stdint.h: 
